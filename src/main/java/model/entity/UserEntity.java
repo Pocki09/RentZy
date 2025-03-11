d0bf1db595b1b0fd1f9c3a9032b8e582e59e56ae
Pocki09 <mha5042109@gmail.com>
2025-03-11 20:26:50 +0700

    thêm entity user

---
+++ src/main/java/model/entity/UserEntity.java
@@ -0,0 +1,23 @@
+package model.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Document(collation = "user")
+public class UserEntity {
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+    private String role; // user, owner, admin
+    private String fullName;
+    private String phone;
+    private String avatar;
+    private Date createdAt;
+    private Date updatedAt;
+}
