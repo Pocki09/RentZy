5229ae6b20f700dffd6a63363986553d13f74d11
Pocki09 <mha5042109@gmail.com>
2025-03-11 19:05:16 +0700

    cấu trúc thư mục

---
+++ src/test/java/com/rentzy/RentzyRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentzyRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
